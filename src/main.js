b62aab544ee921af0b5824ceea8903d31400f061
王允 <1093511773@qq.com>
2022-01-10 17:13:52 +0800

    完成用户列表功能开发

--- src/main.js
+++ src/main.js
@@ -7,6 +7,14 @@ import './assets/css/global.css'
 import axios from 'axios'
 // 配置请求的根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// axios请求拦截
+axios.interceptors.request.use(config => {
+  // 为请求头对象，添加TOKEN验证的 Authorization 字段
+  console.log(config)
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+  // 最后必须 return config
+})
 Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
