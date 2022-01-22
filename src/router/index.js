d418eee1ed7400d2098809855509e554f18e906a
王允 <1093511773@qq.com>
2022-01-22 15:55:52 +0800

    123

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,6 @@ import Users from '../components/user/Users.vue'
 import Rights from '../components/power/Rights.vue'
 import Roles from '../components/power/Roles.vue'
 import Cate from '../components/goods/Cate.vue'
-import Params from '../components/goods/Params.vue'
 
 Vue.use(VueRouter)
 
@@ -30,8 +29,7 @@ const router = new VueRouter({
         { path: '/users', component: Users },
         { path: '/rights', component: Rights },
         { path: '/roles', component: Roles },
-        { path: '/categories', component: Cate },
-        { path: '/params', component: Params }
+        { path: '/categories', component: Cate }
       ]
     }
   ]
